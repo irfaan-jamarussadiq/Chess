93062eaf258bf48bd1b2ab1224f290e892a2fcc7
Irfaan <irfaanj9@gmail.com>
2021-05-19 17:22:22 -0400

    pawn tests

--- tests/PawnTests.java
+++ tests/PawnTests.java
@@ -1,20 +1,21 @@
 package tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import pieces.Pawn;
+import pieces.Piece;
+import pieces.Player;
 import board.BoardModel;
-import board.Game;
-import board.GameState;
-import pieces.*;
 
 public class PawnTests {
-    public static Piece pawn = Pawn(6, 4, Player.WHITE);
+    public static Piece pawn = new Pawn(6, 4, Player.WHITE);
     public static BoardModel model = new BoardModel();
 
     @Test
-    public testPawnPush() {
+    public void testPawnPushUpdatesLocation() {
         assertTrue(model.pieceAt(6, 4) instanceof Pawn);
+        assertTrue(model.pieceAt(4, 4).isEmpty());
         model.movePiece(pawn, 4, 4);
         assertTrue(model.pieceAt(4, 4) instanceof Pawn);
     }
