920738dabd60e56e7d2b535ada43a885c7bb5948
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-16 17:59:19 -0500

    Add new piece definitions again

--- pieces/IllegalPieceException.java
+++
@@ -1,9 +0,0 @@
-package pieces;
-
-public class IllegalPieceException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-	public IllegalPieceException(String string) {
-		super(string);
-	}
-}
---
+++ pieces/Piece.java
@@ -0,0 +1,11 @@
+package pieces;
+
+class Piece {
+	private PieceType type;
+	private PieceColor color;
+
+	public Piece(PieceType type, PieceColor color) {
+		this.type = type;
+		this.color = color;
+	}
+}
