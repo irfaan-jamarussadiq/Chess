04f03c41f9775de8cc998d6a47b1cc24342df491
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-16 18:54:43 -0500

    Extend abstract class for NormalMove

---
+++ board/NormalMove.java
@@ -0,0 +1,19 @@
+class NormalMove extends Move {
+	boolean isValid(Board board) {
+		Piece pieceToMove = board.pieceAt(startRank, startFile); 
+		if (pieceToMove == null) {
+			return false;
+		}
+
+		Piece destination = board.pieceAt(endRank, endFile);
+		return destination == null;
+	}
+
+	void execute(Board board) {
+		if (isValid(board)) {
+			Piece pieceToMove = board.pieceAt(startRank, startFile);
+			board.removePiece(startRank, startFile); 
+			board.addPiece(piece, endRank, endFile);
+		}
+	}
+}
