808c0de8e8ae2d43ee9843d8372e5d2121d1be0e
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-16 18:53:37 -0500

    Redefine Move interface as abstract class

---
+++ board/Move.java
@@ -0,0 +1,22 @@
+package board;
+
+abstract class Move {
+	private int startRank, startFile, endRank, endFile;
+	
+	Move(int startRank, int startFile, int endRank, int endFile) {
+		this.startRank = startRank;
+		this.startFile = startFile;
+		this.endRank = endRank;
+		this.endFile = endFile;
+	}
+
+	boolean withinBoard(Board board) {
+		return startRank >= 1 && startRank <= Board.SIZE
+			&& startFile >= 1 && startFile <= Board.SIZE
+			&& endRank >= 1 && endRank <= Board.SIZE
+			&& endFile >= 1 && endFile <= Board.SIZE; 
+	}
+
+	abstract boolean isValid(Board board);
+	abstract void execute(Board board);
+}
