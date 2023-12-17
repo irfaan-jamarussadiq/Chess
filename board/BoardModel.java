84242377f7970fffbd33c067b28fcf35e93764b7
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-16 20:54:33 -0500

    Add method for getting piece board character and rearrange methods in BoardModel class

--- board/BoardModel.java
+++ board/BoardModel.java
@@ -1,302 +1,269 @@
 package board;
 
 public class BoardModel {
-    
-    public static final int SIZE = 8;
-    private Piece[][] board;
 
-    public BoardModel() {
-	if (SIZE < 8) {
-		throw new IllegalStateException();
+	public static final int SIZE = 8;
+	private Piece[][] board;
+
+	public BoardModel() {
+		if (SIZE < 8) {
+			throw new IllegalStateException();
+		}
+
+		board = new Piece[SIZE][SIZE];
+
+		int start = SIZE / 2 - 4;
+		int end = SIZE / 2 + 4;
+
+		PieceType[] pieceRow = { 
+			PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP, 
+			PieceType.QUEEN, PieceType.KING,
+			PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP 
+		};
+
+		for (int file = start; file < end; file++) {
+			board[start + 1][file] = new Piece(PieceType.PAWN, PieceColor.WHITE); 		
+			board[end - 1][file] = new Piece(PieceType.PAWN, PieceColor.BLACK); 		
+			board[start][file] = new Piece(pieceRow[file], PieceColor.WHITE);
+			board[end][file] = new Piece(pieceRow[file], PieceColor.BLACK);
+		}
 	}
 
-        board = new Piece[SIZE][SIZE];
-
-	int start = SIZE / 2 - 4;
-	int end = SIZE / 2 + 4;
-
-	PieceType[] pieceRow = { 
-		PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP, 
-		PieceType.QUEEN, PieceType.KING,
-		PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP 
-	};
-
-	for (int file = start; file < end; file++) {
-		board[start + 1][file] = new Piece(PieceType.PAWN, PieceColor.WHITE); 		
-		board[end - 1][file] = new Piece(PieceType.PAWN, PieceColor.BLACK); 		
-		board[start][file] = new Piece(pieceRow[file], PieceColor.WHITE);
-		board[end][file] = new Piece(pieceRow[file], PieceColor.BLACK);
+	public Piece pieceAt(int rank, int file) {
+		return board[rank - 1][file - 1];
 	}
-    }
 
-    public void movePieceIfValid(Piece piece, int newRank, int newFile) {
-        if (isValidMove(piece, newRank, newFile)) {
-            movePiece(piece, newRank, newFile);
-        }
-    }
+	void addPiece(Piece piece, int rank, int file) {
+		board[rank - 1][file - 1] = piece;
+	}
 
-    public boolean isValidMove(Piece piece, int newRank, int newFile) {
-        boolean validPieceMove = piece.isValidMove(newRank, newFile);
-        Player squarePlayer = pieceAt(newRank, newFile).getPlayer();
-        boolean notFriendly = squarePlayer != piece.getPlayer();
-        boolean castlingMove = isCastlingMove(piece, newRank, newFile);
-        
-        if (castlingMove && notFriendly) {
-            return true;
-        }
+	void removePiece(int rank, int file) {
+		board[rank - 1][file - 1] = null;
+	}
 
-        Move move = new Move(newRank, newFile, piece);
-        boolean inMoveList = piece.getMoveList(this).contains(move);
-        return validPieceMove && notFriendly && inMoveList;
-    }
+	public void movePieceIfValid(Piece piece, int newRank, int newFile) {
+		if (isValidMove(piece, newRank, newFile)) {
+			movePiece(piece, newRank, newFile);
+		}
+	}
 
-    public void castle(Piece piece, int newRank, int newFile) {
-        if (newRank != piece.getRank() || !(piece instanceof King)
-        || kingIsInCheck(piece.getPlayer())) {
-            return;
-        }
+	public boolean isValidMove(Piece piece, int newRank, int newFile) {
+		boolean validPieceMove = piece.isValidMove(newRank, newFile);
+		Player squarePlayer = pieceAt(newRank, newFile).getPlayer();
+		boolean notFriendly = squarePlayer != piece.getPlayer();
+		boolean castlingMove = isCastlingMove(piece, newRank, newFile);
 
-        King king = (King) piece;
-        if (newFile == king.getFile() + 2 && kingCanCastleOO(king)) {
-            movePiece(king, newRank, newFile);
-            Rook rook = (Rook) pieceAt(newRank, 7);
-            movePiece(rook, newRank, newFile - 1);
-        }
-    }
+		if (castlingMove && notFriendly) {
+			return true;
+		}
 
-    public boolean isCastlingMove(Piece piece, int newRank, int newFile) {
-        if (!(piece instanceof King)) {
-            return false;
-        }
+		Move move = new Move(newRank, newFile, piece);
+		boolean inMoveList = piece.getMoveList(this).contains(move);
+		return validPieceMove && notFriendly && inMoveList;
+	}
 
-        King king = (King) piece;
-        return isCastlingOOMove(king, newRank, newFile)
-            || isCastlingOOOMove(king, newRank, newFile);
-    }
+	public void castle(Piece piece, int newRank, int newFile) {
+		if (newRank != piece.getRank() || !(piece instanceof King)
+				|| kingIsInCheck(piece.getPlayer())) {
+			return;
+		}
 
-    public boolean isCastlingOOMove(King king, int newRank, int newFile) {
-        int castlingRank = (king.getPlayer() == WHITE) ? 7 : 0;
-        if (king.getRank() != castlingRank || king.getRank() != newRank) {
-            return false;
-        }
+		King king = (King) piece;
+		if (newFile == king.getFile() + 2 && kingCanCastleOO(king)) {
+			movePiece(king, newRank, newFile);
+			Rook rook = (Rook) pieceAt(newRank, 7);
+			movePiece(rook, newRank, newFile - 1);
+		}
+	}
 
-        return newFile == king.getFile() + 2 && kingCanCastleOO(king);
-    }
+	public boolean isCastlingMove(Piece piece, int newRank, int newFile) {
+		if (!(piece instanceof King)) {
+			return false;
+		}
 
-    public boolean isCastlingOOOMove(King king, int newRank, int newFile) {
-        int castlingRank = (king.getPlayer() == WHITE) ? 7 : 0;
-        if (king.getRank() != castlingRank || king.getRank() != newRank) {
-            return false;
-        }
+		King king = (King) piece;
+		return isCastlingOOMove(king, newRank, newFile)
+			|| isCastlingOOOMove(king, newRank, newFile);
+	}
 
-        return newFile == king.getFile() - 2 && kingCanCastleOOO(king);
-    }
+	public boolean isCastlingOOMove(King king, int newRank, int newFile) {
+		int castlingRank = (king.getPlayer() == WHITE) ? 7 : 0;
+		if (king.getRank() != castlingRank || king.getRank() != newRank) {
+			return false;
+		}
 
-    public boolean playerCanCastle(Player player) {
-        King king = findKing(player);
-        return kingCanCastleOO(king) || kingCanCastleOOO(king);
-    }
+		return newFile == king.getFile() + 2 && kingCanCastleOO(king);
+	}
 
-    public boolean kingCanCastle(boolean isShortCastle, King king) {
-        Player player = king.getPlayer();
-        int rank = king.getRank();
-        int file = king.getFile();
-        int castlingRank = (player == WHITE) ? 7 : 0;
-        int castlingFile = isShortCastle ? 7 : 0;
-        Piece rook = pieceAt(castlingRank, castlingFile);
-        boolean isRook = rook instanceof Rook && rook.getPlayer() == player;
-        boolean empty = castlingSquaresAreEmpty(isShortCastle, rank, file);
-        return isRook && rank == castlingRank && file == 4 && empty;
-    }
+	public boolean isCastlingOOOMove(King king, int newRank, int newFile) {
+		int castlingRank = (king.getPlayer() == WHITE) ? 7 : 0;
+		if (king.getRank() != castlingRank || king.getRank() != newRank) {
+			return false;
+		}
 
-    private boolean castlingSquaresAreEmpty(boolean isShortCastle, int kingRank,
-        int kingFile) {
-        
-        int firstEmptySquareFile = isShortCastle ? kingFile + 1 : kingFile - 1;
-        int secondEmptySquareFile = isShortCastle ? kingFile + 2 : kingFile - 2;
-        if (!isShortCastle && !pieceAt(kingRank, kingFile - 3).isEmpty()) {
-            return false;
-        }
+		return newFile == king.getFile() - 2 && kingCanCastleOOO(king);
+	}
 
-        return pieceAt(kingRank, firstEmptySquareFile).isEmpty()
-            && pieceAt(kingRank, secondEmptySquareFile).isEmpty();
-    }
+	public boolean playerCanCastle(Player player) {
+		King king = findKing(player);
+		return kingCanCastleOO(king) || kingCanCastleOOO(king);
+	}
 
-    public boolean kingCanCastleOO(King king) {
-        return kingCanCastle(true, king);
-    }
+	public boolean kingCanCastle(boolean isShortCastle, King king) {
+		Player player = king.getPlayer();
+		int rank = king.getRank();
+		int file = king.getFile();
+		int castlingRank = (player == WHITE) ? 7 : 0;
+		int castlingFile = isShortCastle ? 7 : 0;
+		Piece rook = pieceAt(castlingRank, castlingFile);
+		boolean isRook = rook instanceof Rook && rook.getPlayer() == player;
+		boolean empty = castlingSquaresAreEmpty(isShortCastle, rank, file);
+		return isRook && rank == castlingRank && file == 4 && empty;
+	}
 
-    public boolean kingCanCastleOOO(King king) {
-        return kingCanCastle(false, king);
-    }
+	private boolean castlingSquaresAreEmpty(boolean isShortCastle, int kingRank,
+			int kingFile) {
 
-    public void movePiece(Piece piece, int newRank, int newFile) {
-        removePiece(piece.getRank(), piece.getFile());
-        piece.setRank(newRank);
-        piece.setFile(newFile);
-        addPiece(piece);
+		int firstEmptySquareFile = isShortCastle ? kingFile + 1 : kingFile - 1;
+		int secondEmptySquareFile = isShortCastle ? kingFile + 2 : kingFile - 2;
+		if (!isShortCastle && !pieceAt(kingRank, kingFile - 3).isEmpty()) {
+			return false;
+		}
 
-        if (piece instanceof Pawn && (piece.getPlayer() == WHITE && newRank == 0)
-            || (piece.getPlayer() == BLACK && newRank == 7)) {
-                removePiece(newRank, newFile);
-                addPiece(new Queen(newRank, newFile, piece.getPlayer()));
-        }
-    }
+		return pieceAt(kingRank, firstEmptySquareFile).isEmpty()
+			&& pieceAt(kingRank, secondEmptySquareFile).isEmpty();
+	}
 
-    public Piece pieceAt(int rank, int file) {
-        return board[rank][file];
-    }
+	public boolean kingCanCastleOO(King king) {
+		return kingCanCastle(true, king);
+	}
 
-    public boolean isWithinBoard(int rank, int file) {
-        boolean rankInBoard = rank >= 0 && rank < SIZE;
-        boolean fileInBoard = file >= 0 && file < SIZE;
-        return rankInBoard && fileInBoard;
-    }
+	public boolean kingCanCastleOOO(King king) {
+		return kingCanCastle(false, king);
+	}
 
-    void addPiece(Piece piece) {
-        board[piece.getRank()][piece.getFile()] = piece;
-    }
+	public void movePiece(Piece piece, int newRank, int newFile) {
+		removePiece(piece.getRank(), piece.getFile());
+		piece.setRank(newRank);
+		piece.setFile(newFile);
+		addPiece(piece);
 
-    void removePiece(int rank, int file) {
-        board[rank][file] = new Empty(rank, file, UNDEFINED);
-    }
+		if (piece instanceof Pawn && (piece.getPlayer() == WHITE && newRank == 0)
+				|| (piece.getPlayer() == BLACK && newRank == 7)) {
+			removePiece(newRank, newFile);
+			addPiece(new Queen(newRank, newFile, piece.getPlayer()));
+		}
+	}  
 
-   
-    public King findKing(Player player) {
-        for (Piece[] row : board) {
-            for (Piece piece : row) {
-                if (piece instanceof King && player == piece.getPlayer()) {
-                    return (King) piece;
-                }
-            }
-        }
+	public King findKing(Player player) {
+		for (Piece[] row : board) {
+			for (Piece piece : row) {
+				if (piece instanceof King && player == piece.getPlayer()) {
+					return (King) piece;
+				}
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    public boolean noMovesAvailable(Player player) {
-        for (Piece[] row : board) {
-            for (Piece piece : row) {
-                if (piece.getPlayer() == player) {
-                    for (Move move : piece.getMoveList(this)) {
-                        if (!moveWouldCauseCheck(move, player)) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
+	public boolean noMovesAvailable(Player player) {
+		for (Piece[] row : board) {
+			for (Piece piece : row) {
+				if (piece.getPlayer() == player) {
+					for (Move move : piece.getMoveList(this)) {
+						if (!moveWouldCauseCheck(move, player)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    public boolean moveWouldCauseCheck(Move move, Player player) {
-        BoardModel copy = deepCopy();
-        int rank = move.getRank();
-        int file = move.getFile();
-        Piece piece = copy.pieceAt(move.getPiece().getRank(), move.getPiece().getFile());
-        copy.movePieceIfValid(piece, rank, file);
-        boolean isCheck = copy.kingIsInCheck(player);
-        if (isCheck) {
-            piece.getMoveList(this).remove(move);
-        }
-        return isCheck;
-    }
+	public boolean moveWouldCauseCheck(Move move, Player player) {
+		BoardModel copy = deepCopy();
+		int rank = move.getRank();
+		int file = move.getFile();
+		Piece piece = copy.pieceAt(move.getPiece().getRank(), move.getPiece().getFile());
+		copy.movePieceIfValid(piece, rank, file);
+		boolean isCheck = copy.kingIsInCheck(player);
+		if (isCheck) {
+			piece.getMoveList(this).remove(move);
+		}
+		return isCheck;
+	}
 
-    public boolean kingIsInCheck(Player player) {
-        King king = findKing(player);
-        return squareAttacked(player, king.getRank(), king.getFile());
-    }
+	public boolean kingIsInCheck(Player player) {
+		King king = findKing(player);
+		return squareAttacked(player, king.getRank(), king.getFile());
+	}
 
-    public boolean squareAttacked(Player player, int rank, int file) {
-        Piece piece = pieceAt(rank, file);
-        Bishop fakeBishop = new Bishop(rank, file, player);
-        Rook fakeRook = new Rook(rank, file, player);
-        Knight fakeKnight = new Knight(rank, file, player);
-        return fakePieceMoveListHasPiece(piece, fakeKnight)
-            || fakePieceMoveListHasPiece(piece, fakeBishop)
-            || fakePieceMoveListHasPiece(piece, fakeRook);
-    }
+	public boolean squareAttacked(Player player, int rank, int file) {
+		Piece piece = pieceAt(rank, file);
+		Bishop fakeBishop = new Bishop(rank, file, player);
+		Rook fakeRook = new Rook(rank, file, player);
+		Knight fakeKnight = new Knight(rank, file, player);
+		return fakePieceMoveListHasPiece(piece, fakeKnight)
+			|| fakePieceMoveListHasPiece(piece, fakeBishop)
+			|| fakePieceMoveListHasPiece(piece, fakeRook);
+	}
 
-    private boolean fakePieceMoveListHasPiece(Piece piece, Piece fake) {
-        for (Move move : fake.getMoveList(this)) {
-            Piece attacker = pieceAt(move.getRank(), move.getFile());
-            ArrayList<Move> attackList = attacker.getMoveList(this);
-            for (Move m : attackList) {
-                if (m.getRank() == piece.getRank()
-                && m.getFile() == piece.getFile()) {
-                    return true;
-                }
-            }
-        }
+	private boolean fakePieceMoveListHasPiece(Piece piece, Piece fake) {
+		for (Move move : fake.getMoveList(this)) {
+			Piece attacker = pieceAt(move.getRank(), move.getFile());
+			ArrayList<Move> attackList = attacker.getMoveList(this);
+			for (Move m : attackList) {
+				if (m.getRank() == piece.getRank()
+						&& m.getFile() == piece.getFile()) {
+					return true;
+				}
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public boolean isStaleMate() {
-        return isStaleMateHelper(WHITE) && isStaleMateHelper(BLACK);
-    }
+	public boolean isStaleMate() {
+		return isStaleMateHelper(WHITE) && isStaleMateHelper(BLACK);
+	}
 
-    private boolean isStaleMateHelper(Player player) {
-        return noMovesAvailable(player) && !kingIsInCheck(player);
-    }
+	private boolean isStaleMateHelper(Player player) {
+		return noMovesAvailable(player) && !kingIsInCheck(player);
+	}
 
-    public boolean isCheckMate(Player player) {
-        return noMovesAvailable(player) && kingIsInCheck(player);
-    }
+	public boolean isCheckMate(Player player) {
+		return noMovesAvailable(player) && kingIsInCheck(player);
+	}
 
-    public BoardModel deepCopy() {
-        BoardModel newModel = new BoardModel();
-        newModel.clear();
-        for (Piece[] row : board) {
-            for (Piece piece : row) {
-                newModel.addPiece(piece.deepCopy());
-            }
-        }
+	public BoardModel deepCopy() {
+		BoardModel newModel = new BoardModel();
+		newModel.clear();
+		for (Piece[] row : board) {
+			for (Piece piece : row) {
+				newModel.addPiece(piece.deepCopy());
+			}
+		}
 
-        return newModel;
-    }
+		return newModel;
+	}
 
-    public void clear() {
-        clearRanks(0, 7);
-    }
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		for (Piece[] row : board) {
+			for (Piece piece : row) {
+				char letter = (piece == null) ? '.' : piece.getBoardCharacter();
+				result.append(letter + " ");
+			}
 
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder();
-        for (Piece[] row : board) {
-            for (Piece piece : row) {
-                char letter = pieceToLetter(piece);
-                result.append(String.valueOf(letter) + ' ');
-            }
+			result.append('\n');
+		}
 
-            result.append('\n');
-        }
+		return result.toString();
+	}
 
-        return result.toString();
-    }
-
-    // Returns the letter a piece is represented by in algebraic chess notation.
-    private char pieceToLetter(Piece piece) {
-        // Get the first letter of the piece's class name
-        char letter = piece.getClass().getSimpleName().charAt(0);
-
-        // A Knight's letter in algebraic chess notation is 'N', not 'K',
-        // because King already uses 'K'.
-        if (piece instanceof Knight) {
-            letter = 'N';
-        }
-
-        // Lowercase letters are for white pieces, and uppercase for black.
-        if (piece.getPlayer() == WHITE) {
-            letter = Character.toLowerCase(letter);
-        }
-
-        if (piece.isEmpty()) {
-            letter = '.';
-        }
-
-        return letter;
-    }
 }
 
--- pieces/Piece.java
+++ pieces/Piece.java
@@ -8,4 +8,32 @@ class Piece {
 		this.type = type;
 		this.color = color;
 	}
+
+	public char getBoardCharacter() {
+		char character = ' ';
+		switch(type) {
+			case PAWN:
+				character = 'p';
+				break;
+			case ROOK:
+				character = 'r';
+				break;
+			case KNIGHT:
+				character = 'n';
+				break;
+			case BISHOP:
+				character = 'b';
+				break;
+			case QUEEN:
+				character = 'q';
+				break;
+			case KING:
+				character = 'k';
+				break;
+			default:
+				break;
+		}
+		
+		return (color == PieceColor.WHITE) ? character : Character.toUpperCase(character);
+	}		
 }
