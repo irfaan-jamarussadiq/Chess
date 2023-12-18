88753ecea3dab3e09db96b7b308227f323273905
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-17 20:12:30 -0500

    Allow script to test multiple classes

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-className=$1
-javac -d target -cp target:lib/junit-platform-console-standalone-1.6.0.jar "tests/$className.java"
-java -jar lib/junit-platform-console-standalone-1.6.0.jar --class-path target --select-class "tests.$className" 
+for var in "$@"
+do
+	javac -d target -cp target:lib/junit-platform-console-standalone-1.6.0.jar "tests/$var.java"
+	java -jar lib/junit-platform-console-standalone-1.6.0.jar --class-path target --select-class "tests.$var" 
+done
