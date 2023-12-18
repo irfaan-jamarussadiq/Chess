20a665cc5ae1351a77ed7f376f2e88506904c865
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-17 20:24:28 -0500

    Add test for normal pawn move

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -9,10 +9,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoardTest {
+
 	@Test
-	public void testInitialBoardSetUp() {
+	public void testPieceAccess() {
 		BoardModel board = new BoardModel();
-		// Check rooks are in correct locations
 		assertEquals(board.pieceAt(1, 1), new Piece(PieceType.ROOK, PieceColor.WHITE));
+	}
+
+	@Test
+	public void testNormalMove() {
+		BoardModel board = new BoardModel();
+		board.movePiece(2, 4, 4, 4);
+		assertEquals(board.pieceAt(4, 4), new Piece(PieceType.PAWN, PieceColor.WHITE));
+		assertEquals(board.pieceAt(2, 4), null);
 	}
 }
