f5aa152b9f55444a7855a79977eaaea08583e844
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-21 17:25:01 -0500

    Fix bug related to move generation for Rook and Bishop

--- src/board/pieces/BishopMoveGenerator.java
+++ src/board/pieces/BishopMoveGenerator.java
@@ -7,20 +7,14 @@ import board.BoardModel;
 import board.Move;
 
 public class BishopMoveGenerator implements MoveGenerator {
-	private BoardModel board;
-
-	public BishopMoveGenerator(BoardModel board) {
-		this.board = board;
-	}
-
-	public List<Move> getMoves(int rank, int file) {
+	public List<Move> generateMoves(BoardModel board, int rank, int file) {
 		List<Move> moves = new ArrayList<>(16);
 		
 		Piece currentPiece = board.pieceAt(rank, file);
 		int[][] directions = { { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }; 
 		for (int[] direction : directions) {
 			int endRank = rank + direction[0];
-			int endFile = rank + direction[1];
+			int endFile = file + direction[1];
 
 			while (endRank >= 1 && endRank <= BoardModel.SIZE && endFile >= 1 && endFile <= BoardModel.SIZE) {
 				Piece potentialEnemy = board.pieceAt(endRank, endFile);
--- src/board/pieces/RookMoveGenerator.java
+++ src/board/pieces/RookMoveGenerator.java
@@ -7,20 +7,14 @@ import board.BoardModel;
 import board.Move;
 
 public class RookMoveGenerator implements MoveGenerator {
-	private BoardModel board;
-	
-	public RookMoveGenerator(BoardModel board) {
-		board = new BoardModel();
-	}
-
-	public List<Move> getMoves(int rank, int file) {
+	public List<Move> generateMoves(BoardModel board, int rank, int file) {
 		List<Move> moves = new ArrayList<>(16);
 
 		Piece currentPiece = board.pieceAt(rank, file);
 		int[][] directions = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } }; 
 		for (int[] direction : directions) {
 			int endRank = rank + direction[0];
-			int endFile = rank + direction[1];
+			int endFile = file + direction[1];
 
 			while (endRank >= 1 && endRank <= BoardModel.SIZE && endFile >= 1 && endFile <= BoardModel.SIZE) {
 				Piece potentialEnemy = board.pieceAt(endRank, endFile);
