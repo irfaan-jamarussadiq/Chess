c8bca01b790ac515f796ece2afc746fe57e98c73
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-21 17:21:48 -0500

    Remove unused method from CaptureMoveStrategy

--- src/board/CaptureMoveStrategy.java
+++ src/board/CaptureMoveStrategy.java
@@ -3,19 +3,11 @@ package board;
 import pieces.Piece;
 
 class CaptureMoveStrategy implements MoveStrategy {
-	private Piece capturedPiece;
-
 	public void move(BoardModel board, Move move) {
 		Piece pieceToMove = board.pieceAt(move.getStartRank(), move.getStartFile());
 		Piece pieceAtDestination = board.pieceAt(move.getEndRank(), move.getEndFile());
 
 		board.removePiece(move.getStartRank(), move.getStartFile());
 		board.addPiece(pieceToMove, move.getEndRank(), move.getEndFile());
-		
-		capturedPiece = pieceAtDestination;
-	}
-
-	public Piece getCapturedPiece() {
-		return capturedPiece;
-	}
+	}	
 }	
