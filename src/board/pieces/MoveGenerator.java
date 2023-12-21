c0e89b1700e3782f170925971c1e083fe3b5926f
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-21 17:25:38 -0500

    Rename move generator method and add method for captures

--- src/board/pieces/MoveGenerator.java
+++ src/board/pieces/MoveGenerator.java
@@ -1,9 +1,20 @@
 package pieces;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import board.Move;
+import board.BoardModel;
 
 public interface MoveGenerator {
-	List<Move> getMoves(int rank, int file);
+	List<Move> generateMoves(BoardModel board, int rank, int file);	
+
+	default List<Move> generateCaptures(BoardModel board, int rank, int file) {
+		Piece piece = board.pieceAt(rank, file);
+		return generateMoves(board, rank, file)
+			.stream()
+			.filter(move -> piece.isEnemyOf(board.pieceAt(move.getEndRank(), move.getEndFile()))) 
+			.collect(Collectors.toList());
+	}
+
 }
