8df0619437da2478cdb1083fd004cc69a2b40096
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-24 22:13:33 -0500

    Add methods for getting piece, pawn, and enPassant starting ranks for each player color

--- src/board/pieces/PieceColor.java
+++ src/board/pieces/PieceColor.java
@@ -1,5 +1,45 @@
 package pieces;
 
+import board.BoardModel;
+
 public enum PieceColor {
-	WHITE, BLACK
+	WHITE {
+		public int getPieceStartingRank() {
+			return 1;
+		}
+
+		public int getPawnStartingRank() {
+			return 2;
+		}
+
+		public int getEnPassantStartingRank() {
+			return 5;
+		} 
+
+		public int getPawnDirection() {
+			return 1;
+		}
+	}, 
+	BLACK {
+		public int getPieceStartingRank() {
+			return BoardModel.SIZE;
+		}
+
+		public int getPawnStartingRank() {
+			return BoardModel.SIZE - 1;
+		}
+
+		public int getEnPassantStartingRank() {
+			return 4;
+		} 
+
+		public int getPawnDirection() {
+			return -1;
+		}
+	};
+
+	public abstract int getPieceStartingRank(); 
+	public abstract int getPawnStartingRank(); 
+	public abstract int getEnPassantStartingRank(); 
+	public abstract int getPawnDirection(); 
 }
