a5fb91fe7ec4367a136a28b53142969704f6ba33
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-25 22:13:14 -0500

    Check if move is valid before playing move and keep track of move history

--- src/board/Game.java
+++ src/board/Game.java
@@ -1,16 +1,32 @@
 package board;
 
+import java.util.List;
+import java.util.Stack;
+
+import pieces.Piece;
+import pieces.PieceColor;
+import pieces.MoveGenerator;
+import pieces.MoveGeneratorFactory;
 import pieces.KingMoveGenerator;
 
-class Game {
+public class Game {
 	private BoardModel board;
+	private MoveValidator validator;
+	private Stack<Move> moveHistory; 
 
-	public Game() {	
-		board = new BoardModel();
+	private static Player WHITE_PLAYER = new Player(PieceColor.WHITE);
+	private static Player BLACK_PLAYER = new Player(PieceColor.BLACK);
+	private Player currentPlayer;
+
+	public Game(BoardModel board) {	
+		this.board = board;
+		this.validator = new MoveValidator(board);
+		this.moveHistory = new Stack<>();
+		this.currentPlayer = WHITE_PLAYER; 
 	}
 
 	public boolean kingIsInCheck(Player player) {
-		return board.squareIsAttacked(player.getKingRank(), player.getKingFile());
+		return false;
 	}	
 
 	public boolean kingIsInCheckmate(Player player) {
@@ -21,19 +37,35 @@ class Game {
 		return !kingIsInCheck(player) && kingHasNoMoves(player);
 	}
 
-	private boolean kingHasNoMoves(Player player) {
-		KingMoveGenerator generator = new KingMoveGenerator(board); 
-		List<Move> kingMoves = generator.getMoves(player.getKingRank(), player.getKingFile());
-		if (!player.getKingHasMoved()) {
-			kingMoves.addAll(generator.getCastlingMoves(player.getKingRank(), player.getKingFile()));	
-		}
+	public void playMove(int startRank, int startFile, int endRank, int endFile) {
+		Piece pieceToMove = board.pieceAt(startRank, startFile);
+		Piece pieceAtDestination = board.pieceAt(endRank, endFile);
+		Move move = new Move(startRank, startFile, endRank, endFile);	
 
+		MoveStrategy strategy = MoveStrategyFactory.createMoveStrategy(board, move);
+
+		if (validator.isValidMove(currentPlayer, strategy, move) && currentPlayer.getColor() == pieceToMove.getColor()) {
+			board.move(strategy, move);
+			moveHistory.push(move);
+			currentPlayer = (currentPlayer == WHITE_PLAYER) ? BLACK_PLAYER : WHITE_PLAYER;
+		}
+	}
+
+	private boolean kingHasNoMoves(Player player) {
+		KingMoveGenerator generator = new KingMoveGenerator(); 
+		Location kingLocation = player.getKingLocation();
+		List<Move> kingMoves = generator.getMoves(kingLocation.getRank(), kingLocation.getFile());
 		for (Move move : kingMoves) {
-			if (!squareAttacked(move.getEndRank(), move.getEndFile())) {
-				return false;
-			}
+			// TODO ...
 		}
 
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		String player = (currentPlayer == WHITE_PLAYER) ? "WHITE" : "BLACK";	
+		String playerString = String.format("Current player: %s", player);
+		return playerString + "\n" + board.toString();
 	}
 }
