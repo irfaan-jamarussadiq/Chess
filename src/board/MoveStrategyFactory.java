edf5f0b7dda47b3160f1fff55635f89281043c69
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-25 22:08:28 -0500

    Cycle through possible move strategies to find valid move

---
+++ src/board/MoveStrategyFactory.java
@@ -0,0 +1,22 @@
+package board;
+
+public class MoveStrategyFactory {
+
+	private static MoveStrategy[] strategies = {
+		new NormalMoveStrategy(),
+		new CaptureMoveStrategy(),
+		new EnPassantMoveStrategy(),
+		new ShortCastlingMoveStrategy(),
+		new LongCastlingMoveStrategy()
+	};
+
+	public static MoveStrategy createMoveStrategy(BoardModel board, Move move) {
+		for (MoveStrategy strategy : strategies) {
+			if (strategy.isValidMove(board, move)) {
+				return strategy;
+			}
+		}		
+
+		throw new UnsupportedOperationException("Could not find move strategy for move.");
+	}
+}
