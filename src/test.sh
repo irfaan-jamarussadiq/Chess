c674cd20b05fcbfc6b2b0982fc8fdf7be67415e3
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2023-12-30 08:38:37 -0500

    Add class compilation to test script

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+javac -d target -cp target:lib/junit-platform-console-standalone-1.6.0.jar tests/*.java board/*.java board/pieces/*.java
+
 for var in "$@"
 do
 	javac -d target -cp target:lib/junit-platform-console-standalone-1.6.0.jar "tests/$var.java"
